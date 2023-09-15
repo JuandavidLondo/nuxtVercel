20f588861c5cac43945b37027be833c64f712fa9
JuandavidLondo <101955432+JuandavidLondo@users.noreply.github.com>
2023-09-15 15:35:23 -0500

    feat: login vercel

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <center>
+      <h1>Bienvenidos a la app de tareas</h1>
+      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+      <h3>Login</h3>
+
+      <v-card
+      height="230"
+      width="450"
+    >
+      <div>
+
+      <v-responsive    class="mx-auto"    max-width="344" >
+      <v-text-field class="my-custom-text-field" label="Email address"  placeholder=""  type="email" variant="solo-filled" padding-top="100"></v-text-field>
+      </v-responsive>
+           
+      <v-responsive    class="mx-auto"    max-width="344"  >
+      <v-text-field label="Password" type="password" variant="solo-filled"></v-text-field>
+    
+      </v-responsive>
+      <v-btn variant="tonal">
+      Iniciar
+      </v-btn>
+
+
+    </div>
+  </v-card>
+
+      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
+<br>
+<br>
+      <NuxtLink to="/tareas">
+        Click para iniciar (Con nuxtLink)
+      </NuxtLink>
+    </center>
+
+  </div>
+</template>
+<style>
+.my-custom-text-field {
+  padding-top: 20px;
+}
+</style>
+<script>
+definePageMeta({
+  layout: "blank",
+});
+</script>
